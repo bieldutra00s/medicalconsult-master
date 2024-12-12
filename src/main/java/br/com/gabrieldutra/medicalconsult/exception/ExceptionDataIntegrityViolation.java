44809adefa471cf4d35d583c5d6f3a0714d98b8f
Gabriel Dutra <bieldutra41@gmail.com>
2024-12-12 00:39:14 -0300

    Add files via upload

---
+++ src/main/java/br/com/gabrieldutra/medicalconsult/exception/ExceptionDataIntegrityViolation.java
@@ -0,0 +1,7 @@
+package br.com.gabrieldutra.medicalconsult.exception;
+
+public class ExceptionDataIntegrityViolation extends RuntimeException{
+    public ExceptionDataIntegrityViolation(String message) {
+        super(message);
+    }
+}
